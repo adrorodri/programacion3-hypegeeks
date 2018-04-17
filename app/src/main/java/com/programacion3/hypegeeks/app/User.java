76164519732d4368df0c759622091dcca4fe5684
camilolagrava <37836396+camilolagrava@users.noreply.github.com>
2018-04-17 05:43:50 -0700

    aqui esta!!

---
+++ app/src/main/java/com/programacion3/hypegeeks/app/User.java
@@ -0,0 +1,18 @@
+package com.programacion3.hypegeeks.app;
+
+public class User {
+
+private String nombtre;
+private String contrasenna;
+private Album album;
+
+    private User(String nombtre,String contrasenna, Album album){
+        this.album=album;
+        this.contrasenna=contrasenna;
+        this.nombtre=nombtre;
+    }
+
+
+
+
+}
