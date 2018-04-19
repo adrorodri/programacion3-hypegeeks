3072f65a65cb0873a30849d7c7bf50d8226ba4fd
Vivian <yukisaki1998@gmail.com>
2018-04-19 16:50:33 -0400

    layout repetidas

--- app/src/main/java/com/programacion3/hypegeeks/app/MainActivity.java
+++ app/src/main/java/com/programacion3/hypegeeks/app/MainActivity.java
@@ -26,18 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
         users = new ArrayList<>();
         passwords = new ArrayList<>();
-        users.add("camilo");   passwords.add("123");
-        users.add("camila");   passwords.add("456");
-        users.add("sergio");   passwords.add("789");
-        users.add("jorge");   passwords.add("1011");
-        users.add("vivian");   passwords.add("1213");
+        users.add("camilo");
+        passwords.add("123");
+        users.add("camila");
+        passwords.add("456");
+        users.add("sergio");
+        passwords.add("789");
+        users.add("jorge");
+        passwords.add("1011");
+        users.add("vivian");
+        passwords.add("1213");
     }
+
     public void clickLogIn(View view) {
         userValue = String.valueOf(enterUser.getText());
         passwordValue = String.valueOf(enterPassword.getText());
 
-        if(users.contains(userValue)) {
-            if(passwords.get(users.indexOf(userValue)).equals(passwordValue)){
+        if (users.contains(userValue)) {
+            if (passwords.get(users.indexOf(userValue)).equals(passwordValue)) {
 
             } else {
                 Toast.makeText(this, "Contrasenia incorrecta", Toast.LENGTH_LONG).show();
@@ -45,12 +51,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "usuario incorrecto", Toast.LENGTH_LONG).show();
         }
-
-        //if(userValue.equals()) {
-            Toast.makeText(this, "Live with honor, die with Glory", Toast.LENGTH_LONG).show();
-      //  }else if (!accountValue.equals("overwatch@gmail.com")){
-            Toast.makeText(this, "Incorret Account", Toast.LENGTH_SHORT).show();
-        //}else if (!passwordValue.equals("123")){
-            Toast.makeText(this, "Incorret Password", Toast.LENGTH_SHORT).show();
-        }
     }
+}
