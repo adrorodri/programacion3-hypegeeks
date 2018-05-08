b55cfb1124ad5c87cd5c8c99583cac998d5df92a
sergiotorricol <37835479+sergiotorricol@users.noreply.github.com>
2018-05-08 09:09:04 -0400

    dfghjk

--- app/src/main/java/com/programacion3/hypegeeks/app/MainActivity.java
+++ app/src/main/java/com/programacion3/hypegeeks/app/MainActivity.java
@@ -1,18 +1,23 @@
 package com.programacion3.hypegeeks.app;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
+//import android.widget.ImageView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.EasyPermissions;
+import pub.devrel.easypermissions.PermissionRequest;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
     EditText enterUser;
     EditText enterPassword;
     String userValue;
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     static final String SHARED_PREFERENCES = "MySharedPreferences";
     static final String KEY_USERNAME = "username";
+
+    private static final int RC_CAMERA_AND_LOCATION = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
         passwords.add("1011");
         users.add("vivian");
         passwords.add("1213");
+
+        String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        EasyPermissions.requestPermissions(
+                new PermissionRequest.Builder(this, RC_CAMERA_AND_LOCATION, perms)
+                        .build());
 
     }
 
@@ -95,4 +108,24 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "PERMISOS ACEPTADOS!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "PERMISOS RECHAZADOS!", Toast.LENGTH_SHORT).show();
+        // Close the app, as we don't have permissions to use it. App may crash!
+        finish();
+    }
 }
+
