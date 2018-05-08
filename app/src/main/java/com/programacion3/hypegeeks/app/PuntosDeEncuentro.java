36ec87f5cea5b0d7886ad914c019189f5c713ada
camilolagrava <37836396+camilolagrava@users.noreply.github.com>
2018-05-07 20:26:38 -0700

    te sirve esto....?

---
+++ app/src/main/java/com/programacion3/hypegeeks/app/PuntosDeEncuentro.java
@@ -0,0 +1,38 @@
+package com.programacion3.hypegeeks.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PuntosDeEncuentro extends AppCompatActivity {
+
+    List<PuntosParaPuntosDeEncuentro> data = new LinkedList<>();
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntos_de_encuentro);
+        recyclerView = findViewById(R.id.recyclerDePuntosDeEncunetro);
+
+        LlenadorDeLista();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        AdaptadorDePuntosDeEncuentros adapter = new AdaptadorDePuntosDeEncuentros(data);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void LlenadorDeLista() {
+
+        for(int i=0;i<15;i++){
+            data.add(new PuntosParaPuntosDeEncuentro("Calle No "+i));
+        }
+    }
+}
---
+++ app/src/main/java/com/programacion3/hypegeeks/app/PuntosParaPuntosDeEncuentro.java
@@ -0,0 +1,19 @@
+package com.programacion3.hypegeeks.app;
+
+class PuntosParaPuntosDeEncuentro {
+
+    private String direccion;
+
+
+    public PuntosParaPuntosDeEncuentro(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
